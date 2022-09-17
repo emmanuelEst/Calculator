c9882bd084fda18541a3e1df5433a1e87700c7fc
Emmanuel Estrella <estrellaemmanuel74@gmail.com>
2022-09-17 16:18:54 -0500

    Add operate function
    Takes an operator symbol and operates on two numbers given

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ const multiply = (a, b) => a * b;
 
 // divides a by b
 const divide = (a, b) => a / b;
+
+function operate(operator, a, b) {
+    if (operator === '+') {
+        return add(a, b);
+    }
+    if (operator === '-') {
+        return subtract(a, b);
+    }
+    if (operator === '*') {
+        return multiply(a, b);
+    }
+    if (operator === '/') {
+        return divide(a, b);
+    }
+}

0cdbf971cde4aad841ae1c478a41c3ea8d0fa94f
Emmanuel Estrella <estrellaemmanuel74@gmail.com>
2022-09-17 15:59:56 -0500

    Add functions for basic operations
    Add, Subtract, Multiply, and Divide

---
+++ script.js
@@ -0,0 +1,11 @@
+// sums a and b
+const add = (a, b) => a + b;
+
+// subtracts b from a
+const subtract = (a, b) => a - b;
+
+// multiplies a and b
+const multiply = (a, b) => a * b;
+
+// divides a by b
+const divide = (a, b) => a / b;
